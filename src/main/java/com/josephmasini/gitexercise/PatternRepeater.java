5f8eff8d656db87d4c7c08493306af2800da66e3
Esther <esther.spiteri.18@um.edu.mt>
2020-03-17 13:45:57 +0100

    implemented array sort for strings

--- src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
+++ src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
@@ -1,9 +1,14 @@
 package com.josephmasini.gitexercise;
 
+import java.util.Arrays;
+
 public class PatternRepeater {
 
     /* Implement method to sort an array of integers */
 
     /* Implement method to sort an array of strings */
-
+    public String[] stringArraySorter(String [] stringArray){
+        Arrays.sort(stringArray);
+        return stringArray;
+    }
 }
