4baddd5b64822801df9da43cedc4da646586bbd8
Esther <esther.spiteri.18@um.edu.mt>
2020-03-17 14:02:29 +0100

    remoevd bracket

--- src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
+++ src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
@@ -7,11 +7,11 @@ public class PatternRepeater {
     /* Implement method to sort an array of integers */
     public int[] intArraySorter(int[] intArray){
         Arrays.sort(intArray);
-        return intArray
+        return intArray;
     }
 
     /* Implement method to sort an array of strings */
-    public String[] stringArraySorter(String [] stringArray){
+    public String[] stringArraySorter(String [] stringArray)
         Arrays.sort(stringArray);
         return stringArray;
     }
