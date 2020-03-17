e74ffa837e13c7c1625b8dacc6b9a2e9b1fbe3fc
Esther <esther.spiteri.18@um.edu.mt>
2020-03-17 13:48:36 +0100

    implemented int array sorter with error

--- src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
+++ src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 public class PatternRepeater {
 
     /* Implement method to sort an array of integers */
+    public int[] intArraySorter(int[] intArray){
+        Arrays.sort(intArray);
+        return intArray
+    }
 
     /* Implement method to sort an array of strings */
     public String[] stringArraySorter(String [] stringArray){
